267481ea85f139422074faa483ae52b9f5583c59
EC2 Default User <ec2-user@ip-172-31-24-100.ap-northeast-1.compute.internal>
2022-07-12 01:08:09 +0000

    [Add]4-9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
 
-before_action:configure_permitted_parameters,if:devise_controller?
+before_action:configure_permitted_parameters,if: :devise_controller?
 
 def after_sign_in_path_for(resource)
   about_path
+end
+
+def after_sign_out_path_for(resource)
+ about_path
 end
 
 
@@ -14,5 +18,3 @@ def configure_permitted_parameters
 end
 
 end
-
-
